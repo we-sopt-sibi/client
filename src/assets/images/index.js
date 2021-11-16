050204b4e139c2a84135a47495b299e517dd0688
younyikim <rlakuku1221@gmail.com>
2021-11-16 14:21:07 +0900

    :tada: feat : Article Header Style, add image files/index.js

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,1 +1,3 @@
 // export { default as Header } from "./common/Header";
+export { default as ArticleProfileWriter } from "../images/img_profile_writer_100dp.svg";
+export { default as Dot } from "../images/dot.svg";
