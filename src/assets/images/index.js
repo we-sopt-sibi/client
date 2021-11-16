f5f349d243d9829c8959963c80978e3891e574ad
younyikim <rlakuku1221@gmail.com>
2021-11-16 17:21:36 +0900

    :construction: init : assests 파일 추가 및 index 추가

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,3 +1,5 @@
 export { default as drawerProfile } from "./drawerProfile.png";
-export { default as ArticleProfileWriter } from "./img_profile_writer_100dp.svg";
+export { default as ArticleProfileWriter } from "./articleProfileWriter.svg";
 export { default as Dot } from "./dot.svg";
+export { default as ArticleBodyImg1 } from "./articleBodyImg1.svg";
+export { default as ArticleBodyImg2 } from "./articleBodyImg2.svg";
