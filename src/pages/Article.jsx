20e87fb55ac266638188f23150529785c5d6b5fd
soryeongk <soryeongk.kr@gmail.com>
2021-11-16 14:43:51 +0900

    :bug: fix: conflict 방지를 위해 되돌려두기

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -1,21 +1,15 @@
 import { ArticleBody, ArticleBodyFooter, ArticleComment, ArticleFooter, ArticleHeader } from "../components";
-import styled from "styled-components";
 
-const Article = ({ setShowDrawer }) => {
+const Article = () => {
   return (
-    <Wrapper onClick={() => setShowDrawer(false)}>
+    <>
       <ArticleHeader />
       <ArticleBody />
       <ArticleBodyFooter />
       <ArticleComment />
       <ArticleFooter />
-    </Wrapper>
+    </>
   );
 };
 
 export default Article;
-
-const Wrapper = styled.main`
-  /* width: 100%;
-  height: 100%; */
-`;
