75a7dfacdef02d2d1bc2dc4d44219e3b45c245bd
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2021-11-18 20:39:06 +0900

    :tada: feat: Header 틀 완성

--- src/components/articleList/ListHeader.jsx
+++ src/components/articleList/ListHeader.jsx
@@ -3,26 +3,33 @@ import styled from "styled-components";
 import ArrowIcon from "../../assets/icons/arrow.svg";
 
 const ListHeader = () => {
+  const tagData = [
+    "IT",
+    "인공지능",
+    "스마트폰",
+    "아이패드",
+    "인스타그램",
+    "아이패드",
+    "페이스북",
+    "구글",
+    "스마트폰",
+    "애플",
+    "아이패드",
+    "UX",
+    "아이폰",
+    "모바일",
+    "VR",
+    "UI",
+    "UI",
+    "아이패드",
+  ];
   return (
     <StyledWrapper>
       <div>
         <div className="tags">
-          <button>It</button>
-          <button>인공지능</button>
-          <button>스마트폰</button>
-          <button>아이패드</button>
-          <button>페이스북</button>
-          <button>구글</button>
-          <button>스마트폰</button>
-          <button>애플</button>
-          <button>아이패드</button>
-          <button>UX</button>
-          <button>아이폰</button>
-          <button>모바일</button>
-          <button>VR</button>
-          <button>UI</button>
-          <button>UI</button>
-          <button>아이패드</button>
+          {tagData.map((tags, idx) => (
+            <button key={`tag-${idx}`}>{tags}</button>
+          ))}
         </div>
         <div className="more">
           <span>더보기</span>
@@ -59,9 +66,23 @@ const StyledWrapper = styled.div`
   .tags button {
     margin-right: 0.8rem;
     font-size: ${({ theme }) => theme.fontSizes.caption};
+    font-weight: 300;
     color: ${({ theme }) => theme.colors.gray600};
     border-radius: 1.4rem;
-    line-height: 1.7rem;
-    padding: 0.4rem 1.05rem;
+    line-height: 1;
+    padding: 0.4rem 1rem;
+    letter-spacing: -0.04em;
+    align-items: center;
+  }
+
+  .more {
+    display: flex;
+    justify-content: center;
+    line-height: 1.8;
+    color: ${({ theme }) => theme.colors.gray600};
+  }
+
+  .more img {
+    margin-left: 0.3rem;
   }
 `;
