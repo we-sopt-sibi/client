8c3e176d4722c38a22a694eb0174b0c69503b9b3
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2021-11-26 01:59:04 +0900

    :tada: css: ListHeader 수정 및 마무리

--- src/components/articleList/ListHeader.jsx
+++ src/components/articleList/ListHeader.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import ArrowIcon from "../../assets/icons/arrow.svg";
 
 const ListHeader = () => {
@@ -24,6 +24,7 @@ const ListHeader = () => {
     "아이패드",
   ]);
   const [added, setAdded] = useState(false);
+  const [folded, setFolded] = useState("더보기");
 
   const addTags = () => {
     setTagData((currentArray) => [
@@ -58,6 +59,7 @@ const ListHeader = () => {
       "VR",
     ]);
     setAdded(true);
+    setFolded("접기");
   };
 
   const removeTags = () => {
@@ -67,6 +69,7 @@ const ListHeader = () => {
       return newArray;
     });
     setAdded(false);
+    setFolded("더보기");
   };
   return (
     <StyledWrapper>
@@ -76,8 +79,8 @@ const ListHeader = () => {
             <button key={`tag-${idx}`}>{tags}</button>
           ))}
         </TagStyled>
-        <MoreStyled onClick={added ? removeTags : addTags} className="more">
-          <span>더보기</span>
+        <MoreStyled onClick={added ? removeTags : addTags} added={added}>
+          <div>{folded}</div>
           <img src={ArrowIcon} alt="arrow" />
         </MoreStyled>
       </div>
@@ -89,29 +92,26 @@ export default ListHeader;
 
 const StyledWrapper = styled.div`
   width: 100%;
-  min-height: 7rem;
+  height: 100%;
   background-color: ${({ theme }) => theme.colors.white};
   display: flex;
   justify-content: center;
   align-items: center;
   border-bottom: 1px solid ${({ theme }) => theme.colors.gray300};
-  position: relative;
 
   & > div {
     width: 120rem;
     display: flex;
-    align-items: center;
     justify-content: space-between;
-    min-height: 2.6rem;
   }
 `;
 
 const TagStyled = styled.div`
-  position: absolute;
   display: flex;
   flex-wrap: wrap;
   width: 108.5rem;
-  height: 100%;
+  padding-bottom: 1.6rem;
+  /* min-height: 2.6rem; */
 
   & > button {
     font-size: ${({ theme }) => theme.fontSizes.caption};
@@ -120,22 +120,40 @@ const TagStyled = styled.div`
     border-radius: 1.4rem;
     line-height: 1;
     padding: 0.4rem 1rem;
-    margin: 0.5rem 0.8rem;
+    margin: 0 0.8rem;
+    margin-bottom: 1rem;
     margin-left: 0;
     letter-spacing: -0.04em;
     align-items: center;
+    height: 2.6rem;
   }
 `;
 
 const MoreStyled = styled.div`
-  position: absolute;
-  right: 0;
+  /* width: 4.3rem; */
   display: flex;
   justify-content: center;
+  align-items: center;
+  height: 2.6rem;
   line-height: 1.8;
+  padding-bottom: 0.6rem;
   color: ${({ theme }) => theme.colors.gray600};
 
+  & > div {
+    display: flex;
+    align-items: center;
+    height: 100%;
+  }
+
   & > img {
+    transform: rotate(${({ added }) => added && "180deg"}); // rotate시키는 첫 번쨰 방법
+    ${({ added }) =>
+      added &&
+      css`
+        transform: rotate(180deg);
+      `}; // rotate시키는 두 번째 방법
     margin-left: 0.3rem;
+    width: 1rem;
+    height: 1.1rem;
   }
 `;
