bed2a7e9aae97c7b3c847fdbe328f7057ae94d8e
younyikim <rlakuku1221@gmail.com>
2021-12-01 17:07:02 +0900

    :tada: feat : 댓글 확인 버튼 클릭시 textarea 비우기

--- src/components/article/CommentWrite.jsx
+++ src/components/article/CommentWrite.jsx
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 import { writerProfile } from "../../assets/images";
 import { ReactComponent as EmojiIcon } from "../../assets/icons/emoji.svg";
+import { useRef } from "react";
 
 const CommentWrite = ({ onCommentChange, createComment }) => {
-  const handleSubmit = async () => {
+  const commentRef = useRef();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    commentRef.current.value = "";
     await createComment();
   };
 
@@ -11,16 +15,19 @@ const CommentWrite = ({ onCommentChange, createComment }) => {
     <StyledInputWrapper>
       <img src={writerProfile} alt="" />
       <StyledInput>
-        <textarea
-          name="comment"
-          cols="30"
-          rows="10"
-          onChange={(e) => onCommentChange("content", e.target.value)}
-          placeholder="공감과 응원의 댓글은 작가에게 큰 힘이 됩니다. "></textarea>
-        <div>
-          <EmojiIcon />
-          <button onClick={handleSubmit}>확인</button>
-        </div>
+        <form>
+          <textarea
+            ref={commentRef}
+            name="comment"
+            cols="30"
+            rows="10"
+            onChange={(e) => onCommentChange("content", e.target.value)}
+            placeholder="공감과 응원의 댓글은 작가에게 큰 힘이 됩니다. "></textarea>
+          <div>
+            <EmojiIcon />
+            <button onClick={handleSubmit}>확인</button>
+          </div>
+        </form>
       </StyledInput>
     </StyledInputWrapper>
   );
@@ -61,34 +68,36 @@ const StyledInput = styled.div`
   border: 1px solid ${({ theme }) => theme.colors.gray200};
   width: 100%;
 
-  & > textarea {
-    padding: 1.4rem 2rem;
-    width: 100%;
-    resize: none;
-    border: 0;
-  }
-
-  & > textarea::placeholder {
-    font-size: ${({ theme }) => theme.fontSizes.paragraph2};
-    font-weight: 300;
-    letter-spacing: -0.04em;
-    color: ${({ theme }) => theme.colors.gray600};
-  }
-
-  & > div {
-    display: flex;
-    justify-content: space-between;
-    margin: 0 2rem 1rem 2rem;
-
-    & > button {
-      color: ${({ theme }) => theme.colors.gray600};
-      padding: 0.6rem 2.7rem;
-      border-radius: 1.6rem;
+  & > form {
+    & > textarea {
+      padding: 1.4rem 2rem;
+      width: 100%;
+      resize: none;
+      border: 0;
     }
 
-    & > button:hover {
-      color: ${({ theme }) => theme.colors.main};
-      border-color: ${({ theme }) => theme.colors.main};
+    & > textarea::placeholder {
+      font-size: ${({ theme }) => theme.fontSizes.paragraph2};
+      font-weight: 300;
+      letter-spacing: -0.04em;
+      color: ${({ theme }) => theme.colors.gray600};
+    }
+
+    & > div {
+      display: flex;
+      justify-content: space-between;
+      margin: 0 2rem 1rem 2rem;
+
+      & > button {
+        color: ${({ theme }) => theme.colors.gray600};
+        padding: 0.6rem 2.7rem;
+        border-radius: 1.6rem;
+      }
+
+      & > button:hover {
+        color: ${({ theme }) => theme.colors.main};
+        border-color: ${({ theme }) => theme.colors.main};
+      }
     }
   }
 
